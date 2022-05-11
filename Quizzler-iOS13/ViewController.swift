cf90d629cacfeed9c3e48a7010a598f0cdea050e
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-05-11 10:27:18 +0900

    Quizzler  코드 작성중

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,11 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var questionLable: UILabel!
+    
+    let quiz = [
+          ["Four + Two is eqial to Six.","True"],
+          ["Five - Three is greater than One","True"],
+          ["Three + Eight is less than Ten","False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle //True,False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        if questionNumber + 1 < quiz.count {
+        questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLable.text = quiz[questionNumber][0]
+    }
 }
 
